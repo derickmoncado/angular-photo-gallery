113fa2166a34599256494ca70627b52546dc5cf3
Derick Moncado <derickmoncado@gmail.com>
2021-03-13 19:26:38 -0800

    Fixed double qoute issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public name: string = "Derick Moncado";
+  public name: string = 'Derick Moncado';
 }
