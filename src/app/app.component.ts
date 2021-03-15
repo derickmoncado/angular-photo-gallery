5bdb8b177cfb923c7a07ef69531a1aedd516d466
Derick Moncado <derickmoncado@gmail.com>
2021-03-14 20:13:22 -0700

    Set up static app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public name: string = 'Derick Moncado';
-}
+export class AppComponent {}
