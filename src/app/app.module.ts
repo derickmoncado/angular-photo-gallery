3356b2af446e4fee297b3668de4c0146d248f00d
Derick Moncado <derickmoncado@gmail.com>
2021-03-17 11:53:50 -0700

    Generated modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoListComponent } from './components/photo-list/photo-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, PhotoListComponent],
+  declarations: [AppComponent, PhotoListComponent, ModalComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
